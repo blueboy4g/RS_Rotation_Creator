72b5d3837fb2ad08187d9455dc5451fe53115067
ghost <blueboy4g@hotmail.com>
2025-07-08 12:38:53 -0500

    Fix vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/RS_Rotation_Creator/',
   plugins: [react()],
-})
+});
